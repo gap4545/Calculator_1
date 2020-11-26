b582d53a7afbb6169edda6c2059aa48e89caf865
gap4545 <gap4545@gmail.com>
2020-11-26 11:20:21 -0500

    Add files via upload

---
+++ Calculator.py
@@ -0,0 +1,30 @@
+operation = int(input("""Input Operation.
+1. Add
+2. Subtract
+3. Divide
+4. Multiply
+5. Exponent
+"""))
+#Gets Operation from user, bitch
+
+number1 = int(input("Input first number:")) #Needed int() before input()
+number2 = int(input("Input second number:"))
+total = 0
+
+if operation == 1 :
+    total = number1 + number2
+elif operation == 2 :
+    total = number1 - number2
+elif operation == 3 :
+    total = number1 / number2
+elif operation == 4 :
+    total = number1 * number2
+elif operation == 5 :
+    total = number1 ** number2
+#Does the mathe hoe
+print("Your total is", total)
+#Print the total bitch    
+
+    
+    
+                
